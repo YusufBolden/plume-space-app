7df3a0e4dc1ba8ef64bf7a8262c6e18c55f57115
Yusuf Bolden <ajb2316@columbia.edu>
2025-06-30 01:25:07 -0400

    Configures vite base, deploy script, and BrowserRouter basename for GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from '../src/context/AuthProvider';
-import { PostsProvider } from '../src/context/PostsProvider';
+import { AuthProvider } from './context/AuthProvider';
+import { PostsProvider } from './context/PostsProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
       <PostsProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/plume-space-app">
           <App />
         </BrowserRouter>
       </PostsProvider>
